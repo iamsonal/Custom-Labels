4d6afafd5905bec5c8a918abec32ca96da64bbdb
Sonal Chaudhary <sonal@Sonals-Air.hitronhub.home>
2019-09-29 14:01:48 -0700

    Code refactoring

--- force-app/main/default/lwc/displayLabels/displayLabels.js
+++ force-app/main/default/lwc/displayLabels/displayLabels.js
@@ -1,14 +1,8 @@
 import { LightningElement, track } from "lwc";
 import { getCustomLabels } from "c/customLabelService";
 
-const columns = [
-  { label: "Name", fieldName: "Name" },
-  { label: "Value", fieldName: "Value" }
-];
-
 export default class DisplayLabels extends LightningElement {
   @track data;
-  @track columns = columns;
 
   connectedCallback() {
     getCustomLabels()
